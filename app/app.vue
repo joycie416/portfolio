24c4596563074727e98489bfa70a3434cf072b9c
joycie416 <joycie416@gmail.com>
2026-05-26 19:32:10 +0900

    style: primary, gray 팔레트 추가

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="app">
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+.app {
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  background-color: var(--color-primary-45);
+}
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-full h-full flex">
+    <div class="w-80 h-full bg-gray-01 border-r border-r-border">sidebar</div>
+    <h2>Hello World</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
