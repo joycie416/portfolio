3ab5ef1ed3a28a1f773f7cb750fb08e5f6606869
joycie416 <joycie416@gmail.com>
2026-05-27 18:38:30 +0900

    feat: button 수정, red 팔레트 추가

---
+++ app/pages/components/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col gap-3">
+    <h2>버튼</h2>
+    <div class="space-y-1">
+      <h3>variant</h3>
+      <div class="flex gap-2">
+        <Button>Default</Button>
+        <Button variant="destructive">Destructive</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="outline">Outline</Button>
+        <Button variant="ghost">Ghost</Button>
+      </div>
+    </div>
+    <div class="space-y-1">
+      <h3>size</h3>
+      <div class="flex gap-2">
+        <Button size="sm">Small</Button>
+        <Button>Default</Button>
+        <Button size="lg">Large</Button>
+        <Button size="sm" variant="destructive">Small</Button>
+        <Button variant="destructive">Default</Button>
+        <Button size="lg" variant="destructive">Large</Button>
+      </div>
+      <div class="flex gap-2">
+        <Button size="sm" variant="secondary">Small</Button>
+        <Button variant="secondary">Default</Button>
+        <Button size="lg" variant="secondary">Large</Button>
+        <Button size="sm" variant="outline">Outline</Button>
+        <Button variant="outline">Outline</Button>
+        <Button size="lg" variant="outline">Outline</Button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button } from "~/components/ui/button";
+</script>
