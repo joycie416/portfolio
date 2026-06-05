0e9714a61a14f4557f57f98ce1c06788d6b285d2
joycie416 <joycie416@gmail.com>
2026-06-05 18:37:23 +0900

    fix: features/portfolio/index.ts 정리

--- app/components/features/portfolio/index.ts
+++ app/components/features/portfolio/index.ts
@@ -1,4 +1,3 @@
 export { default as ProfileSection } from "./ProfileSection.vue";
 export { default as SkillSection } from "./SkillSection.vue";
 export { default as ProjectSection } from "./ProjectSection.vue";
-export { default as SkillBadge } from "./SkillBadge.vue";
