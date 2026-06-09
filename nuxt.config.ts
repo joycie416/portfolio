984e6eec3dae0fdfaf1f170020ea495479f3de57
joycie416 <joycie416@gmail.com>
2026-06-09 21:16:47 +0900

    feat: favicon, logo 추가 및 적용

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,4 +28,31 @@ export default defineNuxtConfig({
       prerender: true,
     },
   },
+  app: {
+    head: {
+      title: "Haein's Portfolio",
+      htmlAttrs: { lang: "ko" },
+      link: [
+        { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png",
+        },
+        {
+          rel: "apple-touch-icon",
+          type: "image/png",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+      ],
+    },
+  },
 });
